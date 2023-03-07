cb0f291a75f9c5e2d6c43ebcd93113a39a77f34e
kevin defargoth <kevindu52defargoth@protonmail.com>
2023-03-07 12:48:26 +0100

    debut main.c

---
+++ Raspberry/src/main.c
@@ -0,0 +1,17 @@
+#include "stdlib.h"
+#include "stdio.h"
+#include <driver.h>
+
+enum STATE {
+  WAINTING_ZONE,
+  AWAITING_ORDER,
+  ON_THE_ROAD,
+  WAITING_FOR_INTERSECTION,
+  IN_ALLEY,
+  PICKING_UP,
+  UNLOADING,
+};
+
+int main(int argc, char **argv){
+
+}
