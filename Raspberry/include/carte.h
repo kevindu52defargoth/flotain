811f546334f05cbf59be04d18160cc4b8aaff264
kevin defargoth <kevindu52defargoth@protonmail.com>
2023-03-08 17:21:45 +0100

    corrections types

--- Raspberry/include/carte.h
+++ Raspberry/include/carte.h
@@ -1,24 +1,28 @@
 /* Pour contenir les données sur la carte que doivent parcourir les robots */
 
 #define L_CASE 23.6  /* coté d'une case en cm */
-#define NBRE_CASE_I 9  /* nombre de lignes sur la carte */*
+#define NBRE_CASE_I 9  /* nombre de lignes sur la carte */
 #define NBRE_CASE_J 9  /* nombre de colonnes sur la carte */
+#define NBRE_CASE (NBRE_CASE_I*NBRE_CASE_J)
 
-enum direction_t {
+typedef enum direction{
   N, S, E, W
-};
+} direction_t;
 
-struct intersection_t {
+
+typedef struct intersection{
   direction_t d1;
   direction_t d2;
   int id_ressource;
-};
+} intersection_t;
 
-struct route_t {
+typedef struct route {
   direction_t d;
-};
+} route_t;
 
-union cell_t {
+typedef void allee_t;
+
+typedef union cell {
   intersection_t inter;
   route_t road;
-};
+} cell_t;
