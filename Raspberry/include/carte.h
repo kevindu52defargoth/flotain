eee1815850980918b3738af1b8771afede4d3f54
kevin defargoth <kevindu52defargoth@protonmail.com>
2023-03-12 14:38:57 +0100

    compile
    définition multiple de carte, et qq erreurs

--- Raspberry/include/carte.h
+++ Raspberry/include/carte.h
@@ -34,26 +34,7 @@ typedef union cell {
   route_t road;
 } cell_t;
 
-cell_t* carte[NBRE_CASE_I][NBRE_CASE_J] ;
-
-
-#define ALLEE_i(i) &(cell_t){.inter={N, E, i}}
-#define DEPOT_i(i) &(cell_t){.inter={S, W, i+5}}
-#define ALLEE &(cell_t){.inter={W, E, 0}}
-#define ROUTE(d) &(cell_t){.road={d}}
-
-cell_t* carte[NBRE_CASE_I][NBRE_CASE_J] = {
-  {NULL, NULL, ROUTE(S), ROUTE(W), ROUTE(W), NULL, NULL, NULL, NULL},
-  {ALLEE, ALLEE, DEPOT_i(1), NULL, ALLEE_i(1), ALLEE, ALLEE, ALLEE, ALLEE,},
-  {NULL, NULL, ROUTE(S), NULL, ROUTE(N), NULL, NULL, NULL, NULL},
-  {ALLEE, ALLEE, DEPOT_i(2), NULL, ALLEE_i(2), ALLEE, ALLEE, ALLEE, ALLEE,},
-  {NULL, NULL, ROUTE(S), NULL, ROUTE(N), NULL, NULL, NULL, NULL},
-  {ROUTE(S), ROUTE(W), ROUTE(W), NULL, ALLEE_i(3), ALLEE, ALLEE, ALLEE, ALLEE,},
-  {ROUTE(E), ROUTE(S), NULL, NULL, ROUTE(N), NULL, NULL, NULL, NULL},
-  {ROUTE(S), ROUTE(W), NULL, NULL, ALLEE_i(4), ALLEE, ALLEE, ALLEE, ALLEE,},
-  {ROUTE(E), ROUTE(E), ROUTE(E), ROUTE(E),ROUTE(N), NULL, NULL, NULL, NULL},
-};
-
+extern cell_t* carte[NBRE_CASE_I][NBRE_CASE_J];
 
 cell_t coordones();
 
--- Raspberry/src/carte.c
+++ Raspberry/src/carte.c
@@ -1,7 +1,7 @@
 #include <carte.h>
 
 
-struct coordones next_case(struct coordones coord, direction desire )
+struct coordones next_case(struct coordones coord, direction_t desire )
 {
 switch (desire)
 {
@@ -15,7 +15,7 @@ case E:
     coord.g += 1;
     break;
 case W:
-    coord.g -= 1
+    coord.g -= 1;
     break;
 
 default:
@@ -24,3 +24,20 @@ default:
     
 
 }
+
+#define ALLEE_i(i) &(cell_t){.inter={N, E, i}}
+#define DEPOT_i(i) &(cell_t){.inter={S, W, i+5}}
+#define ALLEE &(cell_t){.inter={W, E, 0}}
+#define ROUTE(d) &(cell_t){.road={d}}
+
+cell_t* carte[NBRE_CASE_I][NBRE_CASE_J] = {
+    {NULL, NULL, ROUTE(S), ROUTE(W), ROUTE(W), NULL, NULL, NULL, NULL},
+    {ALLEE, ALLEE, DEPOT_i(1), NULL, ALLEE_i(1), ALLEE, ALLEE, ALLEE, ALLEE,},
+    {NULL, NULL, ROUTE(S), NULL, ROUTE(N), NULL, NULL, NULL, NULL},
+    {ALLEE, ALLEE, DEPOT_i(2), NULL, ALLEE_i(2), ALLEE, ALLEE, ALLEE, ALLEE,},
+    {NULL, NULL, ROUTE(S), NULL, ROUTE(N), NULL, NULL, NULL, NULL},
+    {ROUTE(S), ROUTE(W), ROUTE(W), NULL, ALLEE_i(3), ALLEE, ALLEE, ALLEE, ALLEE,},
+    {ROUTE(E), ROUTE(S), NULL, NULL, ROUTE(N), NULL, NULL, NULL, NULL},
+    {ROUTE(S), ROUTE(W), NULL, NULL, ALLEE_i(4), ALLEE, ALLEE, ALLEE, ALLEE,},
+    {ROUTE(E), ROUTE(E), ROUTE(E), ROUTE(E),ROUTE(N), NULL, NULL, NULL, NULL},
+};
