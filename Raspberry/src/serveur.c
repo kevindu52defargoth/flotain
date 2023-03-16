10531f1c2b7eae626c063c1cbcc4900b698517e2
kevin defargoth <kevindu52defargoth@protonmail.com>
2023-03-16 10:40:21 +0100

    drangement

--- Raspberry/src/serveur.c
+++ Raspberry/src/serveur.c
@@ -1,22 +1,19 @@
 #include "stdlib.h"
 #include "stdio.h"
-// #include <pthread.h>
-// #include <driver_robot.h>
-// #include <commons.h>
-// #include <main.h>
-// #include <carte.h>
 #include <unistd.h>
-#include<C:\Users\ADERALDO\Desktop\Parcours\Flotte\Flotte\flotain\Raspberry\include\network.h>
-#include<string.h>
+#include <network.h>
+#include <string.h>
 #include <data.h>
 
+#define CHECK(sts, msg) if ((sts)==-1) {perror(msg); exit(-1);}
+
 int main(){
-    socket_t sockEcoute;	// socket d'écoute de demande de connexion d'un client
+	socket_t sockEcoute;	// socket d'écoute de demande de connexion d'un client
 	socket_t sockDial;		// socket de dialogue avec un client
 	buffer_t buff;
 	
 	// sockEcoute est une variable externe
-	sockEcoute = creerSocketEcoute(adrIP, port);
+	sockEcoute = creerSocketEcoute("0.0.0.0", 9092);
 	while(1)	// daemon !
 	{
 		// Accepter une connexion
